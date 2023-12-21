0d25d394f4522a20e1480f5a03d4009245818817
nhinojosa <nhinojosa@southtexascollege.edu>
2023-12-21 11:30:28 -0600

    layouts implemented

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,46 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/Home.vue'
-import MealList from '../views/MealList.vue'
+import MealsByName from '../views/MealsByName.vue'
+import MealsByLetter from '../views/MealsByLetter.vue'
+import MealsByIngredient from '../views/MealsByIngredient.vue'
+import Defaultlayout from '../components/DefaultLayout.vue'
+import GuestLayout from '../components/GuestLayout.vue'
 const routes = [
     {
         path: '/',
-        name: 'home',
-        component: Home
+        component: Defaultlayout,
+        children: [
+            {
+                path: '/',
+                name: 'home',
+                component: Home
+            },
+            {
+                path: '/by-name/:name?',
+                name: 'byName',
+                component: MealsByName
+            },
+            {
+                path: '/by-letter/:letter?',
+                name: 'byLetter',
+                component: MealsByLetter
+            },
+            {
+                path: '/by-ingredient/:ingredient?',
+                name: 'byIngredient',
+                component: MealsByIngredient
+            },
+
+        ]
     },
     {
-        path: '/letter/:letter',
-        name: 'byLetter',
-        component: MealList
-    }
+        path: '/guest',
+        component: GuestLayout,
+        children: [
+
+        ]
+    },
+    
 ];
 
 const router = createRouter({
