5c666816bcae3c729d074f9bd96c764432abd0dc
nhinojosa <nhinojosa@southtexascollege.edu>
2023-12-22 09:48:39 -0600

    functional search working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="flex flex-col p-8">
-        <input type ="text" class="rounded border-2 border-gray-200 w-full" 
-        placeholder="Search for meals"/>
+
 
         <div class="flex justify-center gap-2 mt-2">
             <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters.split('')" v-bind:key="letter">
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,9 +1,30 @@
 <template>
-    <div>
-        By Name
+    <div class="p-8">
+        <input type ="text" v-model="keyword" class="rounded border-2 border-gray-200 w-full" 
+        placeholder="Search for meals" @change="searchMeals"/>
+    </div>
+
+
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+        <div v-for="meal of meals" :key="meal.idMeal" class="bg-white shadow rounded-xl">
+            <img :src="meal.strMealThumb" :alt="meal.strMeal" class="rounded-t-xl" />
+            <h3 class="p-3 font-semibold">{{ meal.strMeal }}</h3>
+            <div class="p-3">
+                <a :href="meal.strYoutube" target="_blank">YouTube</a>
+                <router-link to="/">
+                    view
+                </router-link>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
-
+import {computed, ref} from 'vue'
+import store from '../store'
+const keyword = ref('');
+const meals = computed(()=> store.state.searchedMeals);
+function searchMeals() {
+    store.dispatch('searchMeals', keyword.value)
+}
 </script>
