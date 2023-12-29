53077e29dcc1ff0c1122948ca306b6fcd6ab5442
nhinojosa <nhinojosa@southtexascollege.edu>
2023-12-29 13:32:51 -0600

    fixed Mealsbyletter to use Meals component

--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -5,16 +5,14 @@
         </router-link>
     </div>
     <!-- <pre>{{ meals }}</pre> -->
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-       <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal"/>
-    </div>
+    <Meals :meals="meals" />
 </template>
 
 <script setup>
 import { computed, onMounted, watch } from 'vue';
 import store from '../store';
 import { useRoute } from 'vue-router';
-import MealItem from '../components/MealItem.vue';
+import Meals from '../components/Meals.vue'
 const route = useRoute()
 
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
